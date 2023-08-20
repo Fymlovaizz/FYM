c62136a1f375c7f5c4b69927522306a6e1d6440c
Fymlovaizz <thaitinphat565@gmail.com>
2023-08-21 04:09:25 +0700

    123

---
+++ script.js
@@ -0,0 +1,30 @@
+var menu = document.getElementById("menu"),
+    navbar = document.getElementById("navbar"),
+    menubar = document.getElementById("menubar");
+function dropdowndisplay(resolution) {
+    if (resolution.matches) {
+        menu.style.display = "flex";
+        navbar.style.display = "none";
+    }
+    else {
+        menu.style.display = "none";
+        navbar.style.display = "flex";
+        menubar.style.right = '-230px';
+        menu.style.rotate = 'none';
+    }
+}
+var resolution = window.matchMedia("(max-width: 700px)")
+dropdowndisplay(resolution)
+resolution.addListener(dropdowndisplay)
+menu.style.rotate = 'none';
+menu.addEventListener('click', function () {
+    if (menu.style.rotate == 'none') {
+        menubar.style.right = '0';
+        menu.style.rotate = '180deg';
+    }
+    else {
+        menubar.style.right = '-230px';
+        menu.style.rotate = 'none';
+    }
+
+})
